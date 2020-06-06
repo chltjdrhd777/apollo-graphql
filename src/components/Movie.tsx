b269ffc4fa6262ee1f263a67b720979a04333db4
Anderson <chltjdrhd777@naver.com>
2020-06-06 23:14:04 +0900

    Second commit

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -11,7 +11,7 @@ const Container = styled.div`
   border-radius: 7px;
 `;
 
-interface Bg {
+export interface Bg {
   bg: string;
 }
 
--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 import styled from "styled-components";
+import { Bg } from "../components/Movie";
 
 //useParams = extract the id of present page
 
@@ -30,51 +31,71 @@ const Container = styled.div`
   background-image: linear-gradient(-45deg, #d754ab, #fd723a);
   width: 100%;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
   color: white;
 `;
 
 const Column = styled.div`
   margin-left: 10px;
+  width: 35%;
 `;
 const Title = styled.h1`
-  font-size: 65px;
-  margin-bottom: 15px;
+  font-size: 30px;
+  margin-bottom: 5px;
 `;
 
 const Subtitle = styled.h4`
-  font-size: 35px;
-  margin-bottom: 10px;
+  font-size: 20px;
+  margin-bottom: 5px;
+`;
+
+const Rating = styled.div`
+  font-size: 20px;
+  margin-bottom: 5px;
 `;
 
 const Description = styled.p`
-  font-size: 28px;
+  font-size: 15px;
 `;
 
-const Poster = styled.div`
-  width: 25px;
-  height: 60px;
+const Poster = styled.div<Bg>`
+  width: 26%;
+  height: 40%;
   background-color: transparent;
+  background-image: url(${(Bg) => Bg.bg});
+  background-size: cover;
+  background-position: center center;
 `;
 
 //How to draw information with a prop,
+//! Note!!!
+//? I can't access to the data directly like "<div>data.movie.title</div>"
+//? the reason is that during loading process, the page cannot identify the data
+//? therefore, I should request data after checking conditional things like loading
+
 export default () => {
   const { id } = useParams();
   const idInt = parseInt(id);
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id: idInt },
   });
-
-  console.log(loading, data);
+  console.log(data);
   return (
     <Container>
       <Column>
-        <Title>hi</Title>
-        <Subtitle>sub</Subtitle>
-        <Description>description</Description>
+        <Title>{loading ? "loading..." : data.movie.title}</Title>
+        {!loading && data.movie && (
+          <>
+            <Subtitle>Language : {data.movie.language}</Subtitle>
+            <Rating>Rating : {data.movie.rating}</Rating>
+            <Description>{data.movie.description_intro}</Description>
+          </>
+        )}
       </Column>
-      <Poster></Poster>
+      <Poster
+        bg={data && data.movie ? data.movie.medium_cover_image : ""}
+      ></Poster>
     </Container>
   );
 };
